4526ebf973f70b087cbc0e36e5561a6c99511f5d
Jay Baker <jbaker@roadie.com>
2025-07-03 11:19:56 -0500

    home fix

--- modules/home/wezterm.nix
+++ modules/home/wezterm.nix
@@ -9,7 +9,18 @@
       font_size = 22.0,
       color_scheme = "Catppuccin Mocha",
       hide_tab_bar_if_only_one_tab = true,
-      window_decorations = "RESIZE"
+      window_decorations = "RESIZE",
+
+      keys = {
+        key = 'Home',
+        mods = ''',
+        action = wezterm.action.SendString '\x1b[1~'
+      },
+      {
+        key = 'End',
+        mods = ''',
+        action = wezterm.action.SendString '\x1b[4~'
+      },
     }
     return config
     '';
