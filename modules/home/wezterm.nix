cf78572b576a054995e57827aa92358030a5c7d4
Jay Baker <jbaker@roadie.com>
2025-07-03 11:24:12 -0500

    home fix again

--- modules/home/wezterm.nix
+++ modules/home/wezterm.nix
@@ -3,23 +3,23 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-    local wezterm = require 'wezterm'
     local config = {
       font = wezterm.font("BlexMono Nerd Font Mono"),
       font_size = 22.0,
       color_scheme = "Catppuccin Mocha",
       hide_tab_bar_if_only_one_tab = true,
       window_decorations = "RESIZE",
-
       keys = {
-        key = 'Home',
-        mods = ''',
-        action = wezterm.action.SendString '\x1b[1~'
-      },
-      {
-        key = 'End',
-        mods = ''',
-        action = wezterm.action.SendString '\x1b[4~'
+        {
+          key = 'Home',
+          mods = ''',
+          action = wezterm.action.SendString '\x1b[1~'
+        },
+        {
+          key = 'End',
+          mods = ''',
+          action = wezterm.action.SendString '\x1b[4~'
+        },
       },
     }
     return config
