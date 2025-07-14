eb9dbda99ab7a29e6451398cbd84bb883f61e58d
Jay Baker <jbaker@roadie.com>
2025-07-14 15:11:05 -0500

    adds gppr alias

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -9,12 +9,10 @@
         la = "exa -bghla";
         lt = "exa -T";
         cat = "bat";
-        gp = "git push";
         ".." = "cd ..";
         "..." = "cd ../../";
         "...." = "cd ../../..";
         "....." = "cd ../../../..";
-        #switch = "sudo nixos-rebuild switch --flake /home/jay/git/nix-flake";
         garbage = "nix-collect-garbage -d";
         szsh = "source $HOME/.zshrc";
 
@@ -22,10 +20,13 @@
         k = "kubectl";
         kp = "kubectl get po";
         kd = "kubectl describe";
-        gci = "aws sts get-caller-identity";
-        fp = "git push --set-upstream origin $(git symbolic-ref --short HEAD)";
-        gpmr = "git push -o merge_request.create --set-upstream origin $(git symbolic-ref --short HEAD)";
         kns = "kubectl config set-context --current --namespace=$1";
+
+        gci = "aws sts get-caller-identity";
+
+        ga = "git add .";
+        gp = "git push --set-upstream origin $(git symbolic-ref --short HEAD)";
+        gppr = "git push --set-upstream origin $(git symbolic-ref --short HEAD) && gh pr create --title $(git symbolic-ref --short HEAD)";
       };
 
       envExtra = ''
