6739999fd99348ea829a27e08c5007953cbde3a7
Jay Baker <jbaker@roadie.com>
2025-07-29 13:00:56 -0500

    remove TERM env var

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -33,9 +33,6 @@
         PATH=$HOME/bin:$HOME/.cargo/bin:$HOME/.npm-global/bin:$PATH
         source $HOME/git/context/src/context.sh
         source $HOME/git/kubetools/src/kubetools.sh
-        export TERM=screen-256color
-        bindkey "^[[H" beginning-of-line
-        bindkey "^[[F" end-of-line
       '';
 
       sessionVariables = {
